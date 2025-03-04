ec8173e1459fcbf3c374e0b66fb1cc7ba278ed65
Suren Adzhamoglyan <adgsuren@gmail.com>
2025-03-04 02:22:38 -0700

    added ability to send pdf

--- whatsgate.go
+++ whatsgate.go
@@ -2,6 +2,7 @@ package whatsgate
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"io"
@@ -65,12 +66,87 @@ type Message struct {
 	Body string `json:"body"`
 }
 
+type MessagePDF struct {
+	Type  string `json:"type"`
+	Body  string `json:"body"`
+	Media Media  `json:"media"`
+}
+
+type Media struct {
+	Mimetype string `json:"mimetype"`
+	Data     string `json:"data"`
+	Filename string `json:"filename"`
+}
+
+type MessagePDFRequest struct {
+	WhatsappID string     `json:"WhatsappID"`
+	Async      bool       `json:"async"`
+	Recipient  Recipient  `json:"recipient"`
+	Message    MessagePDF `json:"message"`
+}
+
 func (c *Client) SendMessage(recipientPhone, text string) (MessageResponse, error) {
 	body, err := json.Marshal(MessageRequest{
 		WhatsappID: c.WhatsappID,
 		Async:      false,
 		Recipient:  Recipient{Number: recipientPhone},
 		Message:    Message{Type: "text", Body: text},
+	})
+	if err != nil {
+		return MessageResponse{}, err
+	}
+
+	r, err := http.NewRequest("POST", c.url+"/send", bytes.NewBuffer(body))
+	if err != nil {
+		slog.Error(err.Error())
+		return MessageResponse{}, err
+	}
+	r.Close = true
+
+	req, err := c.httpClient.Do(r)
+	if err != nil {
+		slog.Error(err.Error())
+		return MessageResponse{}, err
+	}
+
+	defer req.Body.Close()
+
+	respBody, err := io.ReadAll(req.Body)
+	if err != nil {
+		slog.Error(err.Error())
+		return MessageResponse{}, err
+	}
+
+	if req.StatusCode != http.StatusOK {
+		return MessageResponse{}, errors.New(req.Status)
+	}
+
+	var message MessageResponse
+
+	err = json.Unmarshal(respBody, &message)
+	if err != nil {
+		slog.Error(err.Error())
+		return MessageResponse{}, err
+	}
+
+	return message, nil
+}
+
+func (c *Client) SendPDF(recipientPhone, text, filename string, pdf io.Reader) (MessageResponse, error) {
+	b, err := io.ReadAll(pdf)
+	if err != nil {
+		return MessageResponse{}, err
+	}
+
+	body, err := json.Marshal(MessagePDFRequest{
+		WhatsappID: c.WhatsappID,
+		Async:      false,
+		Recipient:  Recipient{Number: recipientPhone},
+		Message: MessagePDF{Type: "doc", Body: text, Media: Media{
+			Mimetype: "application/pdf",
+			Data:     base64.StdEncoding.EncodeToString(b),
+			Filename: filename,
+		}},
 	})
 	if err != nil {
 		return MessageResponse{}, err
